059c2cffed636e53eefaa0697e99df7c3e79b62c
xTrayambak <xtrayambak@gmail.com>
2026-05-30 20:37:52 +0530

    add: deps: chroma

--- tests/inputs/google_1.nim
+++
@@ -1,8 +0,0 @@
-import components/style/parser
-import pretty
-
-let parsing =
-  newParser(newParserInput(readFile("tests/inputs/css/google-1.beautified.css")))
-let ss = parsing.parseStylesheet()
-
-print ss
