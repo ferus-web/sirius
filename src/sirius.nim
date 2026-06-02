348fe067969bb1d8172bc8559efb655b91d0074d
xTrayambak <xtrayambak@gmail.com>
2026-06-02 22:16:11 +0530

    fix: sirius: don't print args

--- src/sirius.nim
+++ src/sirius.nim
@@ -1,10 +1,8 @@
 import std/os
 import webview/[core, types], argparser
-import pretty, tables
 
 proc main() {.inline.} =
   let args = parseInput()
-  print args
   if args.command.len < 1:
     quit "Usage: sirius [URL] [flags]"
 
