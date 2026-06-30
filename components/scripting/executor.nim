90b4a5f38d0dd386405b6ff35078e1dad152032d
xTrayambak <xtrayambak@gmail.com>
2026-06-30 19:28:12 +0530

    xxx: scripting: keep `dumpBytecode` on
    It's useful for now, there's nothing else to be said.

--- components/scripting/executor.nim
+++ components/scripting/executor.nim
@@ -39,7 +39,7 @@ proc executeScript*(element: tags.HTMLScriptElement, codeBuffer: string) =
   element.script.rt.opts = InterpreterOpts(
     test262: false,
     repl: false,
-    dumpBytecode: false,
+    dumpBytecode: true,
     insertDebugHooks: true,
     codegen: CodegenOpts(
       elideLoops: false,
