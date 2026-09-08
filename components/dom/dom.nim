c1440c0ee63449a8028708b3a18982c433104e46
xTrayambak <xtrayambak@gmail.com>
2026-09-08 22:59:46 +0530

    add: dom: `EventTarget` implementation
    Just so the JS equivalent has something backing it. Plus it was required
    anyways.

--- components/dom/dom.nim
+++ components/dom/dom.nim
@@ -38,11 +38,12 @@ type
     callback*: js.JSValue
     setter*: bool ## was this set via one of the `on*` setters?
 
-  Node* = ref object of RootObj
+  EventTarget* = ref object of RootObj
+    listeners*: Table[string, seq[EventListener]]
+
+  Node* = ref object of EventTarget
     childList*: seq[Node]
     parentNode* {.cursor.}: Node
-
-    listeners*: Table[string, seq[EventListener]]
 
   CharacterData* = ref object of Node
     data*: string
@@ -75,28 +76,28 @@ type
 
   DocumentFragment* = ref object of Node
 
-func addEventListener*(node: Node, event: string, listener: EventListener) =
+func addEventListener*(target: EventTarget, event: string, listener: EventListener) =
   # specs? never heard of her
-  if not node.listeners.contains(event):
-    node.listeners[event] = @[listener]
+  if not target.listeners.contains(event):
+    target.listeners[event] = @[listener]
     return
 
   if listener.setter:
-    for i, existingListener in node.listeners[event]:
+    for i, existingListener in target.listeners[event]:
       if existingListener.setter:
         # If these are both set via those `on*` setters, overwrite the preexisting one, and be done with it.
-        node.listeners[event][i] = listener
+        target.listeners[event][i] = listener
         return
 
   # Otherwise, append a new EventListener instead
-  node.listeners[event] &= listener
+  target.listeners[event] &= listener
 
-proc dispatchEvent*[T](node: Node, event: string, eventObj: T): bool =
-  if not node.listeners.contains(event):
+proc dispatchEvent*[T](target: EventTarget, event: string, eventObj: T): bool =
+  if not target.listeners.contains(event):
     # JS-land doesn't care about this event for this node.
     return
 
-  for listener in node.listeners[event]:
+  for listener in target.listeners[event]:
     # evil rt routing
     listener.rt.callNoRetval(listener.callback, @[listener.rt.wrap(eventObj)])
 
